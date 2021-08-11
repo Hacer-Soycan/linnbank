b5d10fefc9b5d9ea4436c85548b5bb555025ee98
Hacer Soycan <hcrsycn@gmail.com>
2021-08-11 19:07:31 -0400

    new commit

--- src/package1/sfdsd.java
+++ src/package1/sfdsd.java
@@ -2,7 +2,7 @@ package package1;
 
 public class sfdsd {
     public static void main(String[] args) {
-        System.out.println(dgr);
-        System.out.println(ghfy);
+        System.out.println("dgr");
+        System.out.println("ghfy");
     }
 }
